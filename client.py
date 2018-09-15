2b36be17cd7a71fb53d33ebadafdc9032e54fffc
SimonBerens <sberensnyc@gmail.com>
2018-09-15 18:56:24 -0400

    Updated server config

--- client.py
+++ client.py
@@ -101,7 +101,7 @@ if __name__ == '__main__':
     from argparse import ArgumentParser
 
     parser = ArgumentParser()
-    parser.add_argument('-p', '--port', default=80, type=int, help='port to listen on')
+    parser.add_argument('-p', '--port', default=8080, type=int, help='port to listen on')
     args = parser.parse_args()
     port = args.port
 
